5683d3e36f88345bfb07a3dfaee652989f6093cb
shuva41 <shuvankarray@gmail.com>
2021-11-26 09:26:00 +0530

    Header Stlying: Image added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div class="main-info">
+        <h2>Web Development & Website Promotions</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
