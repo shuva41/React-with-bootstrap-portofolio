52402475fe8d8e5e2f1e0434a05692ea3f31ce05
shuva41 <shuvankarray@gmail.com>
2021-11-27 13:33:18 +0530

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 function App() {
   return (
     <div className="App">
+      <Particles
+        prarms={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shpae: {
+              type: 'circle',
+              stroke: {
+                width: 6,
+                color: '#f9ab00',
+              },
+            },
+          },
+        }}
+      />
       <Navbar />
       <Header />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Typed from 'react-typed';
 
 const Header = () => {
   return (
     <div className="header-wrapper">
       <div class="main-info">
-        <h2>Web Development & Website Promotions</h2>
+        <h1>Web Development & Website Promotions</h1>
+        <Typed
+          className="typed-text"
+          strings={[
+            'Web Design',
+            'Web Development',
+            'Facebook Ads SMM',
+            'Google Ads',
+          ]}
+          typeSpeed={30}
+          backSpeed={50}
+          loop
+        />
+        <a href="#" className="btn-main-offer">
+          Contact Me
+        </a>
       </div>
     </div>
   );
